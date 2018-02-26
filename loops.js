97099e03466839dfda011521048b7fa7812f367f
wendy yu <yu.wendyw@gmail.com>
2018-02-26 02:56:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      return ("I am 1 strange loop.") }
+      i.push ("I am 1 strange loop.") }
     else {
-      return ("I am ${i} strange loops.") }
+      i.push ("I am ${i} strange loops.") }
   return array
   }
 }
